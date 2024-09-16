e24c024ada572260805ca310f8b6f22ffb8539dc
Vanta <gmhs022@gmail.com>
2024-09-15 21:06:28 -0500

    more shit aaaa

--- src/main/java/com/vanta/reactoraddon/tileentity/machine/TileEntityReactorSMR.java
+++ src/main/java/com/vanta/reactoraddon/tileentity/machine/TileEntityReactorSMR.java
@@ -3,6 +3,7 @@ package com.vanta.reactoraddon.tileentity.machine;
 import api.hbm.fluid.IFluidStandardTransceiver;
 import api.hbm.tile.IInfoProviderEC;
 import com.hbm.interfaces.IControlReceiver;
+import com.hbm.inventory.fluid.Fluids;
 import com.hbm.inventory.fluid.tank.FluidTank;
 import com.hbm.tileentity.IGUIProvider;
 import com.hbm.tileentity.TileEntityMachineBase;
@@ -16,10 +17,32 @@ import net.minecraft.world.World;
 
 public class TileEntityReactorSMR extends TileEntityMachineBase implements IControlReceiver, IFluidStandardTransceiver, IGUIProvider, IInfoProviderEC {
 
+    public int temp;    // degrees c
+    public int pressure;// psi
+    public double nFlux;// abstract
+    public int control; // %
+
+    public float cRodCoef; // wooo coefficients!
+    public float voidCoef; // mostly affects boilables, generally negative but graphite increases
+    public float tempCoef;
+    public float presCoef;
+    public float deplCoef;
+
+    public int crCount;
+    public int fuelCount;
+    public int totalFuelReactivity;
+
     public FluidTank[] tanks;
     public TileEntityReactorSMR() {
         super(28);
         this.tanks = new FluidTank[2];
+        this.tanks[0] = new FluidTank(Fluids.COOLANT, 32_000);
+        this.tanks[1] = new FluidTank(Fluids.COOLANT_HOT, 32_000);
+
+    }
+
+    private double getReactivity() {
+        return totalFuelReactivity-cRodCoef*((double) control /100);
     }
 
     @Override
